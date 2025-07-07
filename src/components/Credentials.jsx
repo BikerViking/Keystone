17f0de2f3cd685440f97eb5be331602b302f7ea4
BikerViking <160994481+BikerViking@users.noreply.github.com>
2025-07-07 04:15:53 -0400

    refactor: reposition notary badge

--- src/components/Credentials.jsx
+++ src/components/Credentials.jsx
@@ -9,27 +9,23 @@ export default function Credentials() {
   ];
 
   return (
-    <MotionSection className="container space-y-6">
+    <MotionSection className="relative container space-y-6">
       <h2 className="text-3xl font-serif font-semibold tracking-wide heading-gradient text-silver">
         Credentials
       </h2>
-      <div className="overflow-hidden">
-        <figure className="mx-auto mb-4 w-20 md:float-right md:mb-0 md:mr-4 md:w-24">
-          <img
-            src="/nna-badge.png"
-            alt="Certified NNA Notary Signing Agent 2025 badge"
-            className="w-full"
-          />
-        </figure>
-        <ul className="space-y-4">
-          {credentials.map((cred) => (
-            <li key={cred} className="flex items-start">
-              <CheckIcon className="mr-2 h-5 w-5 text-silver" />
-              <span className="text-platinum">{cred}</span>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <img
+        src="/nna-badge.png"
+        alt="Certified NNA Notary Signing Agent 2025 badge"
+        className="absolute right-4 -top-5 w-20 md:w-24"
+      />
+      <ul className="space-y-4">
+        {credentials.map((cred) => (
+          <li key={cred} className="flex items-start">
+            <CheckIcon className="mr-2 h-5 w-5 text-silver" />
+            <span className="text-platinum">{cred}</span>
+          </li>
+        ))}
+      </ul>
     </MotionSection>
   );
 }
