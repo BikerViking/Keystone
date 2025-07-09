4603d5205cc76596fb246cdd6d87e25cb2d1f2ae
BikerViking <160994481+BikerViking@users.noreply.github.com>
2025-07-09 00:50:44 -0400

    chore: enforce https for svg namespaces

--- src/components/PhoneIcon.jsx
+++ src/components/PhoneIcon.jsx
@@ -2,7 +2,7 @@ export default function PhoneIcon({ className }) {
   return (
     <svg
       className={className}
-      xmlns="http://www.w3.org/2000/svg"
+      xmlns="https://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
       strokeWidth={1.5}
